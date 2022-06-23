9cd3f9c129e198b1ce26827f4629e4aa806ed2de
Duka <ddauletov007@gmail.com>
2022-06-23 17:43:07 +0600

    started to work with process trees

--- testest.py
+++ testest.py
@@ -67,16 +67,65 @@ for i in range(len(new_what)):
         if "text" in new_what[i][j]:
             new_what[i][0], new_what[i][j] = new_what[i][j], new_what[i][0]
 
-
 string123 = ""
 duken = []
 for i in range(len(new_what)):
     for j in range(len(new_what[i])):
-        string123 = string123 + new_what[i][j]+"\n"
+        string123 = string123 + new_what[i][j] + "\n"
     duken.append(string123)
     string123 = ""
 
+ids = []
+for i in range(len(new_what)):
+    for j in range(len(new_what[i])):
+        if "parent_process_id" in new_what[i][j]:
+            ids.append(new_what[i][j])
 
-print("duka")
+strid = ""
+new_ids = []
 
+for i in range(len(ids)):
+    strid = ids[i].split("=")[1].replace(" ", "")
+    new_ids.append(strid)
+    strid = ""
 
+who_ids = []
+for i in range(len(new_what)):
+    for j in range(len(new_what[i])):
+        if "parent_process_id" in new_what[i][j]:
+            who_ids.append(who[i])
+
+new_who_ids = []
+for i in range(len(who_ids)):
+    strwhoid = who_ids[i].split("\n")[0].replace(" ", "")
+    strwhoid1 = strwhoid.split("=")[1].replace(" ", "")
+    new_who_ids.append(strwhoid1)
+    strwhoid=[]
+    strwhoid1=[]
+
+arr_for_ids = []
+for i in range(len(new_ids)):
+    query_body_for_ids = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"what.process_id": new_ids[i]}},
+                    {"match": {"what.text": "A new process has been created"}},
+                    {"match": {"where.computer_name": new_who_ids[i]}}
+                ]
+            }
+        }
+    }
+    result_for_ids = client.search(index="security_events_1_19127", body=query_body_for_ids, size=1000)
+    for hit in result_for_ids['hits']['hits']:
+        str_id = '\n '.join(' = '.join((key, val)) for (key, val) in hit["_source"]["what"].items()) + '\n '.join(
+        ' = '.join((key, val)) for (key, val) in hit["_source"]["who"].items())
+        arr_for_ids.append(str_id)
+
+values_ids, counts_ids = np.unique(arr_for_ids, return_counts=True)
+
+for i in range(len(values_ids)):
+    print(values_ids[i])
+    print(counts_ids[i])
+
+print(arr_for_ids)
